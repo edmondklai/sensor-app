9a3070625121a67e84b1861f300042efadc595ac
Edmond <edmondklai@gmail.com>
2022-08-10 17:37:20 -0500

    feat: Fetch data from backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,17 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
 import Buttons from './Buttons';
 
 import './App.css';
 
 const App = (): JSX.Element => {
-  const [data, setData] = useState<string[]>([]);
-
-  useEffect((): void => {
-  })
+  const [data, setData] = useState<number[]>([]);
 
   return (
     <div className="App">
       <h1>App</h1>
       <Buttons setData={setData}></Buttons>
+      <div>{data}</div>
     </div>
   );
 }
--- frontend/src/Buttons.tsx
+++ frontend/src/Buttons.tsx
@@ -1,23 +1,21 @@
 import React from 'react';
+import { data } from './data';
 
 interface ButtonsProps {
-  setData: React.Dispatch<React.SetStateAction<string[]>>
+  setData: React.Dispatch<React.SetStateAction<number[]>>
 }
 
 const Buttons = ({ setData }: ButtonsProps): JSX.Element => {
 
   const onClick = async (event: React.MouseEvent<HTMLButtonElement>): Promise<void> => {
-
-
     const value = ((event.target) as HTMLButtonElement).value;
-    const data = await fetch(`http://localhost:3001/track-points/${value}`);
-    console.log(data)
-    // setState(data);
+    const data = await (await fetch(`http://localhost:3001/track-points/${value}`)).json();
+    setData(data);
   }
 
   return (
     <div>
-      <button value="e71e1c7e573346368b2c2989e8eee61a" onClick={onClick}>Id 1</button>
+      <button value={data.id1} onClick={onClick}>Id 1</button>
       <button>Id 2</button>
     </div>
   )
