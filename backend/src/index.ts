5eaa325907a093ad17738149fcb478107bff3ec3
Edmond <edmondklai@gmail.com>
2022-08-10 15:23:52 -0500

    feat: Add server to serve points

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,20 +7,19 @@ import { Processor } from './process/Processor';
 // const url = 'https://numina-take-home-interview.s3.us-east-2.amazonaws.com/data.csv';
 const path = './src/data/data.csv';
 
-
-
 // const data = new LocalCsvReader(path).read();
-
-const localCsvReader = new LocalCsvReader(path);
-const sensorProcessor = new SensorProcessor();
-const processor = new Processor(localCsvReader, sensorProcessor);
-processor.process();
-
-const points = processor.getTrackPoints('e71e1c7e573346368b2c2989e8eee61a')
-
-
 const app = express();
 
-app.get('/:trackId', (req: Request, res: Response): void => {
-  res.send('hi');
+app.get('/track-points/:trackId', (req: Request, res: Response): void => {
+  const localCsvReader = new LocalCsvReader(path);
+  const sensorProcessor = new SensorProcessor();
+  const processor = new Processor(localCsvReader, sensorProcessor);
+  processor.process();
+
+  const points = processor.getTrackPoints(req.params.trackId);
+  res.send(points);
+})
+
+app.listen(3001, () => {
+  console.log('listening on 3001')
 })
